19f49446804e1624a194b1160c1af8f9b4429cff
David Pellegrini <david.pellegrini@spoke.com>
2012-07-16 14:58:26 -0700

    Refactored class_attr_spec

--- spec/lib/zitdunyet/class_attr_spec.rb
+++ spec/lib/zitdunyet/class_attr_spec.rb
@@ -2,9 +2,6 @@ require "spec_helper"
 
 describe "Class Attr" do
 
-  before(:all) do
-  end
-
   before(:each) do
     begin
       Object.send(:remove_const, :Baz)
@@ -16,33 +13,11 @@ describe "Class Attr" do
     end
   end
 
-  it "should define class-specific attrs" do
-    class Foo
-      include Zitdunyet::ClassSpecific
-
-      def add(item)
-        self.class.checklist_add item
-        self.class.units_add 1
-      end
-
-      def chklst
-        self.class.checklist
-      end
-
-      def dump
-        puts "Foo#dump: checklist=#{self.class.checklist}, units=#{self.class.units}"
-      end
-    end
-
-    foo = Foo.new
-    foo.dump
-    foo.add("Hello")
-    foo.dump
-    foo.chklst.should have(1).items
-
+  after(:each) do
+    puts "=  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  ="
   end
 
-  it "should support inheritance" do
+  let(:foo) do
     class Foo
       include Zitdunyet::ClassSpecific
 
@@ -64,45 +39,47 @@ describe "Class Attr" do
       end
     end
 
-    foo = Foo.new
-    foo.dump
-    foo.add("Hello")
-    foo.dump
-    foo.chklst.should have(1).items
+    Foo.new
+  end
 
+  let(:baz) do
     class Baz < Foo
       def dump
         puts "Baz#dump: checklist=#{self.class.checklist}, units=#{self.class.units}"
       end
     end
 
-    baz = Baz.new
+    Baz.new
+  end
+
+  it "should initialize with empty array and zeros" do
+    foo.chklst.should be_empty
+    foo.chklst.should have(0).items
+    foo.units.should == 0
+  end
+
+  it "should define class-specific attrs" do
+    foo.dump
+    foo.add("Hello")
+    foo.dump
+    foo.chklst.should have(1).items
+    foo.units.should == 1
+  end
+
+  it "should support inheritance" do
+    foo.dump
+    foo.add("Hello")
+    foo.dump
+    foo.chklst.should have(1).items
+
     baz.dump
     baz.add("World")
     baz.dump
     baz.chklst.should have(2).items
     baz.units.should == 2
 
+    foo.dump
     foo.chklst.should have(1).items
     foo.units.should == 1
-  end
-
-  it "should initialize with empty array and zeros" do
-    class Foo
-      include Zitdunyet::ClassSpecific
-
-      def chklst
-        self.class.checklist
-      end
-
-      def units
-        self.class.units
-      end
-    end
-
-    foo = Foo.new
-    foo.chklst.should be_empty
-    foo.chklst.should have(0).items
-    foo.units.should == 0
   end
 end

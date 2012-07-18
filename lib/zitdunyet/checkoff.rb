854853b6ba744fa0e4f23dc3c7d10b57ec5f3526
David Pellegrini <david.pellegrini@spoke.com>
2012-07-18 01:02:31 -0700

    Roughed in support for staleness, but commented out.

--- lib/zitdunyet/checkoff.rb
+++ lib/zitdunyet/checkoff.rb
@@ -1,6 +1,6 @@
 module Zitdunyet
   class Checkoff
-    attr_accessor :label, :percent, :units, :logic, :hint, :stale, :after, :every, :period
+    attr_accessor :label, :percent, :units, :logic, :hint, :stale, :when
 
     def initialize(label, progress_amount, *opts, &logic)
       # Fill in the required fields
@@ -8,12 +8,21 @@ module Zitdunyet
       assign_progress(progress_amount)
       block_given? ? self.logic = logic : (raise ArgumentError.new "Missing the block for assessing completion")
 
-      # Fill in the optional fields
-      unless opts.empty?
-        options = opts.first
-        self.hint = options.delete(:hint)
-      end
+      # Fill in the optional fields if present
+      assign_options(opts) unless opts.empty?
+    end
 
+    def assign_options(opts)
+      options = opts.first
+      self.hint = options.delete(:hint)
+      #self.stale = options.delete(:stale)
+      #self.when = options.delete(:when)
+      raise ArgumentError.new "Unrecognized options: #{options.inspect}" unless options.empty?
+
+      #if self.stale or self.when
+      #  raise ArgumentError.new "Specified 'stale' without 'when'" unless self.when
+      #  raise ArgumentError.new "Specified 'when' without 'stale'" unless self.stale
+      #end
     end
 
     def assign_progress(progress_amount)
